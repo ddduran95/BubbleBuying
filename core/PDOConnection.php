837dc0a80cdb116422abf28256ccc32fec6bea11
Barajas04 <isma.verin@gmail.com>
2016-11-04 17:50:55 +0100

    bd connection

--- core/PDOConnection.php
+++ core/PDOConnection.php
@@ -2,18 +2,18 @@
 // file: /core/PDOConnection.php
 
 class PDOConnection {
-  private static $dbhost = "127.0.0.1";
+  private static $dbhost = "localhost";
   private static $dbname = "mvcblog";
-  private static $dbuser = "mvcuser";
-  private static $dbpass = "mvcblogpass";
+  private static $dbuser = "root";
+  private static $dbpass = "";
   private static $db_singleton = null;
-  
+
   public static function getInstance() {
     if (self::$db_singleton == null) {
       self::$db_singleton = new PDO(
 	"mysql:host=".self::$dbhost.";dbname=".self::$dbname.";charset=utf8", // connection string
-	self::$dbuser, 
-	self::$dbpass, 
+	self::$dbuser,
+	self::$dbpass,
 	array( // options
 	  PDO::ATTR_EMULATE_PREPARES => false,
 	  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
