fe4c6439586ccfa53df8fd8bf1b5c328dfd6b0eb
ddduran95 <danyel_ou@hotmail.es>
2016-11-29 11:55:21 +0100

    cambios layout menu lateral

--- view/layouts/default.php
+++ view/layouts/default.php
@@ -41,12 +41,33 @@
           <div class="dropdown">
               <i class="fa fa-globe	 fa-2x dropbtn" aria-hidden="false"> </i>
               <div class="dropdown-content">
-                 <a href="index.php?controller=language&amp;action=change&amp;lang=es"><?= i18n("Spanish") ?></a>
+                <a href="index.php?controller=language&amp;action=change&amp;lang=es"><?= i18n("Spanish") ?></a>
                 <a href="index.php?controller=language&amp;action=change&amp;lang=en"><?= i18n("English") ?></a>
               </div>
           </div>
       </div>
     </header>
+    <nav id="mainnavigation">
+        <ul>
+            <li> <a class="btn_menu prioridad_1" href="#"><i class="fa fa-user"></i> Mi Perfil</a></li>
+			<li> <a class="btn_menu prioridad_1" href="#"><i class="fa fa-archive"></i> Mis Productos</a></li>
+			<li> <a class="btn_menu prioridad_1" href="#"><i class="fa fa-arrow-up"></i> Subir Producto</a></li>
+			<li> <a class="btn_menu prioridad_1" href="#"><i class="fa fa-comments"></i> Mis Chats</a></li>
+			<li id="categoriasmenuitem"><a class= "btn_menu prioridad_1" href="#"><i class="fa fa-chevron-down"></i> Categorías</a>
+				<ul>
+					<li><a class= "btn_menu prioridad_2" href="#"><i class="fa fa-laptop"></i> Tecnologia </a></li>
+					<li><a class= "btn_menu prioridad_2" href="#"><i class="fa fa-book"></i> Libros </a></li>
+					<li><a class= "btn_menu prioridad_2" href="#"><i class="fa fa-home"></i> Cosas de casa </a></li>
+					<li><a class= "btn_menu prioridad_2" href="#"><i class="fa fa-gamepad"></i> Consolas </a></li>
+					<li><a class= "btn_menu prioridad_2" href="#"><i class="fa fa-child"></i> Niños </a></li>
+					<li><a class= "btn_menu prioridad_2" href="#"><i class="fa fa-plug "></i> Electrodomesticos </a></li>
+					<li><a class= "btn_menu prioridad_2" href="#"><i class="fa fa-suitcase "></i> Ropa </a></li>
+					<li><a class= "btn_menu prioridad_2" href="#"><i class="fa fa-car "></i> Motor </a></li>
+					<li><a class= "btn_menu prioridad_2" href="#"><i class="fa fa-futbol-o "></i> Deporte </a></li>
+				</ul>
+			</li>
+		</ul>
+    </nav>
       <div id="flash">
 	  <?= $view->popFlash() ?>
       </div>
