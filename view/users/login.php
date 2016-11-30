b51849ad363e3b6f05efc89cd16fad4e15faaacb
ddduran95 <danyel_ou@hotmail.es>
2016-11-30 12:44:26 +0100

    login y registro con estilo

--- view/users/login.php
+++ view/users/login.php
@@ -8,18 +8,20 @@
 ?>
 <article id = "maincontent">
   <div class = "main">
-      <h1><?= i18n("Login") ?></h1>
-      <?= isset($errors["general"])?$errors["general"]:"" ?>
+    <div class="login">
+        <h1><?= i18n("Login") ?></h1>
+        <?= isset($errors["general"])?$errors["general"]:"" ?>
 
-      <form action="index.php?controller=users&amp;action=login" method="POST">
-      <?= i18n("Username")?>: <input type="text" name="username">
-      <?= i18n("Password")?>: <input type="password" name="passwd">
-      <input type="submit" value="<?= i18n("Login") ?>">
-      </form>
+        <form action="index.php?controller=users&amp;action=login" method="POST" class="formulario">
+        <?= i18n("Username")?>: <input type="text" name="username">
+        <?= i18n("Password")?>: <input type="password" name="passwd">
+        <input type="submit" value="<?= i18n("Login") ?>" class=btn-registro>
+        </form>
 
-      <p><?= i18n("Not user?")?> <a href="index.php?controller=users&amp;action=register"><?= i18n("Register here!")?></a></p>
-      <?php $view->moveToFragment("css");?>
-          <link rel="stylesheet" type="text/css" src="css/style2.css">
-      <?php $view->moveToDefaultFragment(); ?>
+        <p><?= i18n("Not user?")?> <a href="index.php?controller=users&amp;action=register"><?= i18n("Register here!")?></a></p>
+        <?php $view->moveToFragment("css");?>
+            <link rel="stylesheet" type="text/css" src="css/style2.css">
+        <?php $view->moveToDefaultFragment(); ?>
     </div>
-  </article>
+  </div>
+</article>
--- view/users/register.php
+++ view/users/register.php
@@ -7,19 +7,26 @@
  $user = $view->getVariable("user");
  $view->setVariable("title", "Register");
 ?>
-<h1><?= i18n("Register")?></h1>
-<form action="index.php?controller=users&amp;action=register" method="POST">
-      <?= i18n("Name")?>: <input type="text" name="name"
-			value="<?= $user->getName() ?>">
-      <?= isset($errors["name"])?$errors["name"]:"" ?><br>
+<article id = "maincontent">
+  <div class = "main">
+    <div class="login">
+      <h1><?= i18n("Sing up!")?></h1>
+      <form action="index.php?controller=users&amp;action=register" method="POST" class="formulario">
+            <?= i18n("Name")?>: <input type="text" name="name"
+      			value="<?= $user->getName() ?>" >
+            <?= isset($errors["name"])?$errors["name"]:"" ?>
 
-      <?= i18n("Alias")?>: <input type="text" name="alias"
-			value="<?= $user->getAlias() ?>">
-      <?= isset($errors["alias"])?$errors["alias"]:"" ?><br>
+            <?= i18n("Alias")?>: <input type="text" name="alias"
+      			value="<?= $user->getAlias() ?>">
+            <?= isset($errors["alias"])?$errors["alias"]:"" ?>
 
-      <?= i18n("Password")?>: <input type="password" name="passwd"
-			value="">
-      <?= isset($errors["passwd"])?$errors["passwd"]:"" ?><br>
+            <?= i18n("Password")?>: <input type="password" name="passwd"
+      			value="">
+            <?= isset($errors["passwd"])?$errors["passwd"]:"" ?>
 
-      <input type="submit" value="<?= i18n("Register")?>">
-</form>
+            <input type="submit" value="<?= i18n("Register")?>" class=btn-registro>
+      </form>
+      <p><?= i18n("Already have an acount?")?> <a href="index.php?controller=users&amp;action=login"><?= i18n("Login here!")?></a></p>
+    </div>
+  </div>
+</article>
